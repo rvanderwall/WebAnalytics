6ca9f0076c1bd5d39f7ed40f7e9f20d5794ea32d
rvanderwall <rvanderwall@yahoo.com>
2013-11-13 21:36:47 -0500

    modify test file

--- Tests.py
+++ Tests.py
@@ -9,28 +9,24 @@ import LogRecord
 
 print "PASS"
 
-USE_SMALL_DATA = True
+USE_SPARSE_DATA = True
+DATAPATH = "M:\MorningBeacon\DigitalAlloyData"
+APACHE_LOG = DATAPATH + "/escweek_sorted.log"
+CDN_LOG = DATAPATH + "/cdnweek_sorted.log"
 
-if USE_SMALL_DATA:
-    APACHE_LOG = "C:/Users/rlv/Desktop/escweek_sorted.log"
-    COLLECTION_NAME = "Digital_Alloy_Small"
-    CDN_LOG = "C:/Users/rlv/Desktop/cdnweek_sorted.log"
-    CDN_LOG = "C:/Users/rlv/Desktop/cdn_small.txt"
-    CDN_COLLECTION_NAME = "Digital_Alloy_CDN_Small"
+if USE_SPARSE_DATA:
+    COLLECTION_NAME = "DA_WebLog_Sparce"
+    CDN_COLLECTION_NAME = "DA_CDNLog_Sparce"
     USE_ROWS = 1000000
-    SKIP_ROWS = 1
-    # SKIP_ROWS = sys.maxint
-else:
-    APACHE_LOG = "C:/Users/rlv/Desktop/escweek_sorted.log"
-    COLLECTION_NAME = "Digital_Alloy"
-    CDN_LOG = "C:/Users/rlv/Desktop/cdnweek_sorted.log"
-    CDN_COLLECTION_NAME = "Digital_Alloy_CDN"
-    USE_ROWS = sys.maxint
-    #SKIP_ROWS = sys.maxint  # Skip all, do only verification.
     SKIP_ROWS = 20  # Only insert every 1 in 20 rows
+else:
+    COLLECTION_NAME = "DA_WebLog"
+    CDN_COLLECTION_NAME = "DA_CDNLog"
+    USE_ROWS = sys.maxint
+    SKIP_ROWS = sys.maxint  # Skip all, do only verification.
 
 
-repo = LogRecordRepository(COLLECTION_NAME)
+ repo = LogRecordRepository(COLLECTION_NAME)
 #import_log_data_to_repo(repo, APACHE_LOG, SKIP_ROWS, USE_ROWS)
 get_simple_stats(repo.collection)
 
