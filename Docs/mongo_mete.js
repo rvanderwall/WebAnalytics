a0d5c7bbec184cdd4abdcf9359986b99b44a8b07
Mete Dizioglu <mete.dizioglu@gmail.com>
2014-02-05 20:50:42 -0500

    Updated some manual mongo scripts.

--- Docs/mongo_mete.js
+++ Docs/mongo_mete.js
@@ -4,9 +4,29 @@ db.DA_WebLog.count({Type:"videos", Description: { $exists: true, $ne:'' }})  //1
 db.DA_WebLog_Videos.count() //2314854
 db.DA_WebLog_Videos.count({Type:"videos", Username: { $exists: true }})
 db.DA_WebLog_Videos.count({Type:"videos", Username: { $ne:'' }})
-db.DA_WebLog_Videos.count({Username: { $ne:'' }})
+db.DA_WebLog_Videos.count({Username: { $ne:'' }})
 db.DA_WebLog_Videos.aggregate({$group:{_id:"$Username",total:{$sum:1}}})
-//db.DA_WebLog_Videos.update({},{$set:{Username:''}}, false, true)
+//db.DA_WebLog_Videos.update({},{$set:{Username:''}}, false, true)
+
+db.DA_WebLog_Videos.count({Username: /admiralmemo/i})
+db.DA_WebLog_Videos.find({Username: 'Eric+Costa'}, {Name:1, TimeOfRequest:1})
+db.DA_WebLog_Videos.find({Username: /eddy/i}, {Name:1, TimeOfRequest:1})
+db.DA_WebLog_Videos.find({Username: /james/i}, {Name:1, TimeOfRequest:1})
+db.DA_WebLog_Videos.find({Username: /admiralmemo/i}, {Name:1, TimeOfRequest:1})
+
+db.DA_WebLog_Videos.distinct('Name',{Username: /admiralmemo/i})
+db.DA_WebLog_Videos.distinct('Name',{Username: /mbe/i})
+
+db.DA_WebLog_Videos.find({Username: /admiralmemo/i}, {Name:1, TimeOfRequest:1})
+db.DA_WebLog_Videos.find({Username: /rartino/i, Name:/Prisoners/i}, {Name:1, TimeOfRequest:1})
+
+
+
+
+
+db.DA_WebLog_Videos.find({Name: '7250-Feed-Dump-100', Username: { $ne:'' }}, {Username:1, TimeOfRequest:1})
+
+
 
 db.DA_WebLog.aggregate({$match:{Type:"videos"}},{$group:{_id:"$Type",total:{$sum:1}}})
 
